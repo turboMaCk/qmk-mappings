e42aa01d6c0bf1a37e1092a7a3f5cf6dcb54fb41
Marek Fajkus <marek.faj@gmail.com>
2023-10-24 10:13:38 +0200

    define makefile

---
+++ Makefile
@@ -0,0 +1,25 @@
+# KB stands for keyboard
+# KM stands for keymap
+KB:=keychron/q8/ansi
+KM:=turboMaCk
+
+# Env override
+QMKCONFIG:=.home/.config/qmk/qmk.ini
+
+# Command flags
+RM:=rm -rf
+CP:=cp -R
+QMK:=qmk --config-file $(QMKCONFIG)
+
+.PHONY: init
+init:
+	$(RM) qmk_firmware/keyboards/$(KB)
+	$(CP) keyboards/$(KB) qmk_firmware/keyboards/$(KB)
+
+.PHONY: compile
+compile: init
+	$(QMK) compile -kb $(KB) -km $(KM)
+
+.PHONY: flash
+flash: init
+	$(QMK) flash -kb $(KB) -km $(KM)
--- shell.nix
+++ shell.nix
@@ -3,4 +3,40 @@ with pkgs;
 mkShell {
   name = "qmk-dev-shell";
   buildInputs = [ qmk ];
+  shellHook = ''
+    FAKEHOME=$(pwd)/.home
+    mkdir -p $FAKEHOME/.config/qmk
+
+    # Generate ini file
+    cat << EOF > $FAKEHOME/.config/qmk/qmk.ini
+    [user]
+    qmk_home = $(pwd)/qmk_firmware
+
+    [config]
+
+    [console]
+
+    [cformat]
+
+    [compile]
+
+    [docs]
+
+    [flash]
+
+    [hello]
+
+    [info]
+
+    [lint]
+
+    [list_keymaps]
+
+    [kle2json]
+
+    [new_keymap]
+
+    [general]
+    EOF
+  '';
 }
